13c2b038284e825b45255c1937a5fe8d89242bcd
Wilton24 <cbasher24@gmail.com>
2023-11-12 16:47:11 +0800

    first commit yeay!

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <UserInput />
+    </>
+  );
+}
